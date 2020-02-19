236da88eedb0fc5d0bce722dd5f43e1cc2e1da16
Qaswar Hussain <47313728+qaswarh@users.noreply.github.com>
2020-02-19 01:06:02 -0800

    Update LTE5G.py

--- LTE5G.py
+++ LTE5G.py
@@ -1,4 +1,8 @@
-file_txt = open('ShUDA.txt', 'r')
+import re
+import tabulate
+
+str_input = '501'
+file_txt = open('GxCCR_U.txt', 'r')
 table = []
 for line in file_txt:
     if ':' in line:
@@ -12,7 +16,7 @@ for line in file_txt:
             tr = [key, value]
             table.append(tr)
 
-print(tabulate.tabulate(table, tablefmt="fancy_grid")))
+print(tabulate.tabulate(table, tablefmt="fancy_grid"))
 
 
 
